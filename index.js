16264f2e2574b903af77d5ba7ca3caffc264d973
Tristan Mason <24866055+tristanmason@users.noreply.github.com>
2020-01-09 21:58:45 -0600

    3.9

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
+const cors = require('cors')
 
+app.use(cors())
 app.use(bodyParser.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :reqData'));
 
@@ -99,7 +101,7 @@ let persons = [
     response.json(person)
   })
   
-  const PORT = 3001
+  const PORT = process.env.PORT || 3001
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
   })
